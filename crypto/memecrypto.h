ab10a6d0f2c7499a6cb99dd3d6f174482548cb0a
Nobody <nobody@example.com>
2022-06-05 14:37:36 +0500

    Memecrypto code cleanup

--- crypto/memecrypto.h
+++ crypto/memecrypto.h
@@ -3,17 +3,10 @@
 
 #include <cstdint>
 
-struct memecrypto_variant {
-    const uint64_t *shuffleTab;
-    const uint32_t *magicTab[4];
-    const uint8_t *magicTab2;
-    const uint8_t *reverseMagicTab2;
-};
-
-extern memecrypto_variant memecrypto_variant0;
-
 void memecrypto_prepare_key(const uint8_t *in, uint8_t *out);
 
-void memecrypto_decrypt(const memecrypto_variant *variant, const uint8_t *key, uint8_t *data);
+void memecrypto_decrypt(const uint8_t *key, uint8_t *data);
+
+void memecrypto_encrypt(const uint8_t *key, uint8_t *data);
 
 #endif //MEMECRYPTO_H
